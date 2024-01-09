6f36ff2b9f26853e3526553dd957ec4bf77f771f
alibinauanov <an3502@nyu.edu>
2024-01-09 20:00:18 +0600

    second commit

--- content.js
+++ content.js
@@ -1,12 +1,19 @@
-const elementClassName = "main-hero-banner__flight-search";
+console.log("ayo this text shows that extension reached to content.js");
 
-function setElementTimeout() {
-  const element = document.querySelector(`.${elementClassName}`);
-  if (element) {
-    setTimeout(() => {
-        document.getElementsByClassName(`.${elementClassName}`);
-    }, 0);
+const elementClassName = "main-hero-banner__flight-search";
+// const elementClassName = "horizontal-flight-search";
+
+function showAllElementsImmediately() {
+  // I had to put the index because there are many elements and have to choose specific one
+  const elements = document.getElementsByClassName(elementClassName);
+
+  // i didn't put the index there because i need to work with all elements inside of "main-hero-banner__flight-search" class. 
+  // i looped all elements
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].style.display = "block";
   }
 }
 
-document.addEventListener("DOMContentLoaded", setElementTimeout);
+showAllElementsImmediately();
+
+// overall, it still doesn't work. i guess it can be a problem of choosing a wrong class. i tried to use some of them, but again, it still doesn't work
